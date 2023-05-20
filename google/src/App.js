e76b2dbb9afad9b9f9de46b9765562e7ed675421
Bryan <bryanborck@gmail.com>
2023-05-20 18:16:40 -0300

    google front feito

--- google/src/App.js
+++ google/src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import Mic from './images/microphone_google.png';
+import { RiYoutubeFill, RiSteamFill, RiWhatsappFill, RiSnapchatFill, RiTwitterFill, RiFacebookCircleFill, RiMediumFill, RiNetflixFill, RiSearchLine } from "react-icons/ri";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Top_app">  
+      </div>
+      <div className="Wrapper"> 
+        <div className="Google_title">
+          <p>
+            <span className="blue">G</span>
+            <span className="red">o</span>
+            <span className="yellow">o</span>
+            <span className="blue">g</span>
+            l
+            <span className="red">e</span>
+          </p>
+        </div>
+        <div className="Search_bar">
+          <RiSearchLine size="24px" color="gray"/>
+          <input className="Input_search"></input>
+          <img className="Mic" src={Mic} alt='mic'></img>
+        </div> 
+        <div className="Icons_bar">
+          <div className="Icon">
+            <div className="Icon_round">
+              <RiYoutubeFill size="4.5vh" color="red"/>
+            </div>
+            <div className="Icon_text">Youtube</div>
+          </div>
+          <div className="Icon">
+            <div className="Icon_round">
+              <RiMediumFill size="4.5vh" color="black"/>
+            </div>
+            <div className="Icon_text">Medium</div>
+          </div>
+          <div className="Icon">
+            <div className="Icon_round">
+              <RiNetflixFill size="4.5vh" color="red"/>
+            </div>
+            <div className="Icon_text">Netflix</div>
+          </div>
+          <div className="Icon">
+            <div className="Icon_round">
+              <RiFacebookCircleFill size="4.5vh" color="blue"/>
+            </div>
+            <div className="Icon_text">Facebook</div>
+          </div>
+        </div> 
+        <div className="Icons_bar">
+        <div className="Icon">
+            <div className="Icon_round">
+              <RiTwitterFill size="4.5vh" color="cyan"/>
+            </div>
+            <div className="Icon_text">Twitter</div>
+          </div>
+          <div className="Icon">
+            <div className="Icon_round">
+              <RiSnapchatFill size="4.5vh" color="yellow"/>
+            </div>
+            <div className="Icon_text">Snapchat</div>
+          </div>
+          <div className="Icon">
+            <div className="Icon_round">
+              <RiWhatsappFill size="4.5vh" color="green"/>
+            </div>
+            <div className="Icon_text">Whatsapp</div>
+          </div>
+          <div className="Icon">
+            <div className="Icon_round">
+              <RiSteamFill size="4.5vh" color="blue"/>
+            </div>
+            <div className="Icon_text">Steam</div>
+          </div>
+        </div> 
+      </div>
+      <div className="Bottom_app">  
+      </div>
     </div>
   );
 }
---
+++ google/src/components/Icon.js
@@ -0,0 +1,10 @@
+import './Icon.css';
+
+function Icon() {
+  return (
+    <div className="Icon">
+    </div>
+  );
+}
+
+export default Icon;
